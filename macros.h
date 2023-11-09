6f2c488964171723409a6a29b210eae5d16f3981
kde-la-c <kdelacolinaplant@gmail.com>
2023-11-09 19:53:41 +0100

    added system condition for macros

--- macros.h
+++ macros.h
@@ -20,14 +20,27 @@
 # define CH_COLLEC	'C'
 # define CH_EXIT	'E'
 
-# define KEY_ESC	53 
-# define KEY_W		13
-# define KEY_A		0
-# define KEY_S		1
-# define KEY_D		2
-# define KEY_UP		126
-# define KEY_LEFT	123
-# define KEY_DOWN	125
-# define KEY_RIGHT	124
+# ifdef __APPLE__
+#  define KEY_ESC	53 
+#  define KEY_W		13
+#  define KEY_A		0
+#  define KEY_S		1
+#  define KEY_D		2
+#  define KEY_UP	126
+#  define KEY_LEFT	123
+#  define KEY_DOWN	125
+#  define KEY_RIGHT	124
+# elif __linux__
+#  define KEY_ESC	65307
+#  define KEY_W		122
+#  define KEY_A		113
+#  define KEY_S		115
+#  define KEY_D		100
+#  define KEY_UP	65362
+#  define KEY_LEFT	65361
+#  define KEY_DOWN	65364
+#  define KEY_RIGHT	65363
+# endif
+
 
 #endif
